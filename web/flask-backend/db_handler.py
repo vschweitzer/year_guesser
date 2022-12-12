0e49b82cd360be9f2165d1c9f72f4b200c7bfc41
Valentin Schweitzer <valentin.schweitzer98@gmail.com>
2022-12-13 00:31:51 +0100

    Complete databank insertion script

--- web/flask-backend/db_handler.py
+++ web/flask-backend/db_handler.py
@@ -2,13 +2,14 @@ import mariadb
 import typing
 import json
 import datetime
+import hashlib
 
 
 class DBHandler:
     def __init__(self, config_path: str) -> None:
         with open(config_path, "r") as config_file:
             self.config: dict = json.load(config_file)
-
+        self.hasher = hashlib.sha256
         self.connection: mariadb.Connection = mariadb.connect(**self.config)
         self.connection.autocommit = True
         # self.cursor: mariadb.Cursor = self.connection.cursor()
@@ -39,7 +40,7 @@ class DBHandler:
                     self.add_full_item(
                         item, page, input_dict[collection_index][item_index][page_index]
                     )
-                    #exit(0)
+                    # exit(0)
 
     def add_collection(self, id: str):
         cursor = self.connection.cursor()
@@ -56,14 +57,63 @@ class DBHandler:
             "SELECT collection_id FROM collections c WHERE(c.collection_id = ?)", (id,)
         )
 
-    def add_full_item(self, item_id: str, page: int, item: dict):
+    def add_full_item(
+        self, item_id: str, page: int, item: dict, collections: list = []
+    ):
         date: datetime.datetime = datetime.datetime.fromisoformat(item["date"])
         date_raw: str = item["date_raw"]
+        image_id: str = self.get_image_id(item_id, page)
         if not self.item_exists(item_id, page):
-            try:
-                self.add_item(id=item_id, page=page, date=date, date_raw=date_raw)
-            except Exception as ex:
-                print(ex)
+            self.add_item(id=item_id, page=page, date=date, date_raw=date_raw)
+        else:
+            print("Skip item insertion")
+
+        if not self.image_exists(item_id, page, image_id):
+            self.add_image(item_id=item_id, page=page, image_id=image_id)
+        else:
+            print("Skip image insertion")
+
+        for collection_id in collections:
+            if not self.collection_item_exists(item_id, page, collection_id):
+                self.add_item_to_collection(item_id, page, collection_id)
+            else:
+                print(f"Skip inserting {item_id}/{page} into {collection_id}")
+
+        for style in item["cite_this"]:
+            if not self.item_citation_exists(item_id, page, style):
+                self.add_item_citation(item_id, page, style, item["cite_this"][style])
+
+    def item_citation_exists(self, item_id: str, page: int, style: str):
+        return self.boolean_selection(
+            "SELECT item_id, page, style FROM cite_as WHERE (item_id = ? AND page = ? AND style = ?)",
+            (item_id, page, style),
+        )
+
+    def add_item_citation(self, item_id: str, page: int, style: str, citation: str):
+        cursor = self.connection.cursor()
+        cursor.execute(
+            "INSERT INTO cite_as (item_id, page, style, citation) VALUES (?, ?, ?, ?)",
+            (item_id, page, style, citation),
+        )
+
+    def collection_item_exists(self, item_id: str, page: int, collection_id: str):
+        return self.boolean_selection(
+            "SELECT item_id, page, collection_id FROM collection_items c WHERE (c.item_id = ? AND c.page = ? AND c.collection_id = ?)",
+            (item_id, page, collection_id),
+        )
+
+    def add_item_to_collection(self, item_id: str, page: int, collection_id: str):
+        if not self.item_exists(item_id, page):
+            raise KeyError(f"Item {item_id}/{page} does not exist")
+
+        if not self.collection_exists(collection_id):
+            raise KeyError(f"Collection {collection_id} does not exist")
+
+        cursor = self.connection.cursor()
+        cursor.execute(
+            "INSERT INTO collection_items (item_id, page, collection_id) VALUES (?, ?, ?)",
+            (item_id, page, collection_id),
+        )
 
     def add_item(self, id: str, page: int, date: datetime.datetime, date_raw: str):
         cursor = self.connection.cursor()
@@ -72,7 +122,6 @@ class DBHandler:
             "INSERT INTO items (item_id, page, date, date_raw) VALUES (?, ?, ?, ?)",
             (id, page, date_string, date_raw),
         )
-
 
     def item_exists(self, id: str, page: int):
         return self.boolean_selection(
@@ -83,16 +132,35 @@ class DBHandler:
             ),
         )
 
+    def add_image(self, item_id: str, page: int, image_id: str, ending: str = "jpeg"):
+        cursor = self.connection.cursor()
+        cursor.execute(
+            "INSERT INTO images (item_id, page, image_id, ending) VALUES (?, ?, ?, ?)",
+            (item_id, page, image_id, ending),
+        )
+
+    def image_exists(
+        self, item_id: str, page: int, image_id: str, ending: str = "jpeg"
+    ):
+        return self.boolean_selection(
+            "SELECT item_id, page, image_id, ending FROM images i WHERE (i.item_id = ? AND i.page = ? AND i.image_id = ? AND i.ending = ?)",
+            (item_id, page, image_id, ending),
+        )
+
     def delete_all_items(self):
         cursor = self.connection.cursor()
         cursor.execute("DELETE FROM items")
+
+    def get_image_id(self, id: str, page: int):
+        unhashed: str = id.strip("/") + "/" + str(page)
+        return self.hasher(unhashed.encode("utf-8")).hexdigest()
 
 
 if __name__ == "__main__":
     db_config_path: str = "./db_access.json"
     handler = DBHandler(db_config_path)
+    handler.delete_all_items()
     for id, page in handler.get_ids():
         print(f"{id} - {page}")
 
-    
     handler.load_from_json("./images.json")

183bb1533a8a13308ee9e6bb31a8530e43ccd707
Valentin Schweitzer <valentin.schweitzer98@gmail.com>
2022-12-14 04:02:42 +0100

    Pass request to views

--- web/python-backend/date_guesser/views.py
+++ web/python-backend/date_guesser/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 
-def show():
+def show(request):
     """
     Show an image and let the user guess the appropriate date.
     """
     pass
 
-def tell():
+def tell(request):
     """
     Tell the user the answer.
     """

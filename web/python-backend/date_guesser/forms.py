8012432f003a39465eb5face716cde9be9906d9a
Valentin Schweitzer <valentin.schweitzer98@gmail.com>
2022-12-18 02:55:42 +0100

    Add form for guessing a year

---
+++ web/python-backend/date_guesser/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Item
+from django.db.models import Min, Max
+
+class YearForm(forms.Form):
+    min_year = Item.objects.all().aggregate(Min("date"))["date__min"].year
+    max_year = Item.objects.all().aggregate(Max("date"))["date__max"].year
+    year_guess = forms.IntegerField(
+        label="Year", min_value=min_year, max_value=max_year
+    )
